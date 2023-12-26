7ba85a04a3c28fdce80cf01754bf08c6202cd2e3
SanketTops <sanket.tops@gmail.com>
2023-12-26 14:31:46 +0530

    String final

--- Module-1/string.py
+++ Module-1/string.py
@@ -1,10 +1,35 @@
-mystr="This is Python!"
+mystr="this is python!"
 
-print(mystr)
+"""print(mystr)
 print(mystr[0])
 print(mystr[-1])
-print(mystr[0:4]) #string slice
+print(mystr[0:4]) #string slice(range)
 print(mystr[5:])
 print(mystr[:4])
 
 print(len(mystr))
+"""
+
+# -------------------------------------------- #
+
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('T','t'))
+#print(mystr.replace('This','That'))
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count('This'))
+#print(mystr.endswith('!'))
+#print(mystr.startswith('That'))
+
+#print(mystr.find('PYTHON'))
+#print(mystr.index('P'))
+
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+print(mystr.title())
