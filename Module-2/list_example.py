15f8d2f70bc6fe28903022b5e8d107ca93607be9
SanketTops <sanket.tops@gmail.com>
2023-12-28 15:02:27 +0530

    Collection - List

---
+++ Module-2/list_example.py
@@ -0,0 +1,57 @@
+data=['c','c++','ds','python','java','php']
+
+"""
+print(data)
+print(data[0])
+print(data[-1])
+print(data[0:3])
+print(data[2:])
+print(data[:4])"""
+
+# --------------------------------------- #
+#print(data)
+"""print(len(data))
+
+if 'python' in data:
+    print("Yes...")
+else:
+    print("Error!")"""
+
+#data[2]='angular'
+#print(data)
+
+"""for i in data:
+    print(i)"""
+
+#print(data.index('java'))
+
+#c=0
+#c++=1
+"""for i in data:
+    print(f"{i}={data.index(i)}")"""
+
+"""n=0
+for i in data:
+    print(f"{i}={n}")
+    n+=1"""
+
+# ------------------------------------ #
+
+#print(data)
+#data.append('react')
+#data.insert(2,'ruby')
+#data.pop()
+#data.pop(1)
+#data.remove('java')
+#del data[0]
+#data.clear()
+#data.reverse()
+#data.sort()
+print(data)
+
+#newdata=data.copy()
+newdata=['HTML','CSS','JS','Bootstrap']
+print(newdata)
+#print(data+newdata)
+data.extend(newdata)
+print(data)
---
+++ Module-2/list_userinput.py
@@ -0,0 +1,13 @@
+data=[]
+
+
+n=int(input("Enter number of elements:")) #n=5
+
+for i in range(n):
+    x=input("Enter your element:") #java, python, c++, php, ds
+    data.append(x)
+
+print(data)
+
+
+
