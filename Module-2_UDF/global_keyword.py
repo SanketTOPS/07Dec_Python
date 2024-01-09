a6161f5f7e4208cefed3b4880277cf5a891efe3f
SanketTops <sanket.tops@gmail.com>
2024-01-09 15:07:27 +0530

    global-local keywords

---
+++ Module-2_UDF/global_keyword.py
@@ -0,0 +1,10 @@
+a=54
+
+print("A=",a)
+
+def getnum():
+    global a
+    #a=98
+    print("A=",a)
+
+getnum()
---
+++ Module-2_UDF/global_local.py
@@ -0,0 +1,15 @@
+#global
+a=34
+b=76
+
+
+
+def getmul():
+    #local
+    a=34
+    b=76
+    print("Mul:",a*b)
+
+getmul()
+print("Sum:",a+b)
+
