30eb11760e700a9d65d0043e4165305c9afb4604
SanketTops <sanket.tops@gmail.com>
2024-03-12 15:01:35 +0530

    CRUD - Insert

---
+++ Django Project/DBProject/myapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+class studData(admin.ModelAdmin):
+    ordering=['id']
+    list_display=['id','name','email','dob','mobile']
+
+
+admin.site.register(studinfo,studData)
---
+++ Django Project/DBProject/myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class studinfo(models.Model):
+    name=models.CharField(max_length=20)
+    email=models.EmailField()
+    dob=models.DateField()
+    mobile=models.BigIntegerField()
+    address=models.TextField()
+
